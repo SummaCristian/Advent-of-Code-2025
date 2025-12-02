1ff33884213c988b9b6e5bd23d5e7c688d13e4e7
Cristian Summa <cristian.summa@mail.polimi.it>
2025-12-02 10:28:04 +0100

    Completed Day 02 - Part 2

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -37,7 +37,14 @@ struct Day02: AdventDay {
 
   /// Solves the second part of the problem
   func part2() -> Any {
-    return "Not implemented yet"
+    var invalidIDs: [Int] = []
+
+    for range in ranges {
+        invalidIDs.append(contentsOf: getAllInvalidIDs(in: range))
+    }
+
+    /// Return the sum of all the invalid IDs
+    return invalidIDs.reduce(0, +)
   }
 
   /// Returns an array containing all the invalid IDs 
@@ -48,8 +55,6 @@ struct Day02: AdventDay {
     let finalValue = range.1
 
     // Only check valid ranges and ones where the numbers have at least 2 digits
-
-
     if finalValue >= initialValue, String(finalValue).count > 1 {
         for id in initialValue...finalValue {
             let stringID = String(id)
@@ -65,6 +70,60 @@ struct Day02: AdventDay {
 
             if firstHalf == secondHalf {
                 invalidIDs.append(id)
+            }
+        }
+
+    }
+
+    return invalidIDs
+  }
+
+  /// Returns an array containing all the invalid IDs 
+  /// found in the specified range, using Part 2 requirements
+  private func getAllInvalidIDs(in range: (Int, Int)) -> [Int] {
+    var invalidIDs: [Int] = []
+    let initialValue = range.0
+    let finalValue = range.1
+
+    // Only check valifd ranges and ones where the numbers have at least 2 digits
+    if finalValue >= initialValue, String(finalValue).count > 1 {
+        for id in initialValue...finalValue {
+            let stringID = String(id)
+
+            let length = stringID.count
+
+            // Check if the string is at least 2 digits long
+            guard length > 1 else { continue }
+            /// Check if the String can be divided in at least 2 parts
+            let maxNumberOfParts = length
+            guard maxNumberOfParts >= 2 else { continue }
+
+            // Iterate over all the possible number of repeated combinations (from 2 to count/2)
+            for partsCount in 2...maxNumberOfParts {
+                // Check if the ID can actually be divided into that number of substrings
+                guard length % partsCount == 0 else { continue }
+
+                let substringLength = length / partsCount
+                
+                // Check if the pattern in the first substring is 
+                // repeated in all the following ones.
+                let pattern = stringID.prefix(substringLength)
+                
+                var isRepeated = true
+                for i in 1..<partsCount {
+                    let start = stringID.index(stringID.startIndex, offsetBy: i * substringLength)
+                    let end = stringID.index(start, offsetBy: substringLength)
+
+                    if stringID[start..<end] != pattern {
+                        isRepeated = false
+                        break
+                    }
+                }
+
+                if isRepeated {
+                    invalidIDs.append(id)
+                    break
+                }
             }
         }
 
