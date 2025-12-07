74b54e832c99a6a4acb011ea0afaadfff2e970a5
Cristian Summa <cristian.summa@mail.polimi.it>
2025-12-07 16:15:39 +0100

    Uploaded Day 07 challenge

---
+++ Sources/Day07.swift
@@ -0,0 +1,16 @@
+import Algorithms
+
+struct Day07: AdventDay {
+  // Save your data in a corresponding text file in the `Data` directory.
+  var data: String
+
+  // Solves the first part of the problem
+  func part1() -> Any {
+    return "Not implemented yet"
+  }
+
+  // Solves the second part of the problem
+  func part2() -> Any {
+    return "Not implemented yet"
+  }
+}
